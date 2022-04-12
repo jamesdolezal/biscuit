ae3e689183d10031968e70210aa1d5826f91e9ee
James <jamesmdolezal@gmail.com>
2022-04-11 23:29:38 -0500

    Refactor for utils.py

--- biscuit/utils.py
+++ biscuit/utils.py
@@ -6,7 +6,7 @@ import numpy as np
 import slideflow as sf
 import matplotlib.colors as colors
 
-from biscuit.errors import *
+from biscuit.errors import MultipleModelsFoundError, ModelNotFoundError
 from biscuit.delong import delong_roc_variance
 from sklearn import metrics
 from scipy import stats
@@ -21,16 +21,31 @@ uncertainty_header = f'{OUTCOME}_uncertainty1'
 y_true_header = f'{OUTCOME}_y_true0'
 y_pred_header = f'{OUTCOME}_y_pred1'
 
-# --- General utility functions ---------------------------------------------------------------------------------------
+
+# --- General utility functions -----------------------------------------------
 
 def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    """Truncates matplotlib colormap."""
+
     new_cmap = colors.LinearSegmentedColormap.from_list(
         'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
         cmap(np.linspace(minval, maxval, n)))
     return new_cmap
 
+
 def get_model_results(path, outcome=None):
-    if outcome is None: outcome = OUTCOME
+    """Reads results/metrics from a trained model.
+
+    Args:
+        path (str): Path to model.
+        outcome (str, optional): Outcome name. If none, uses default
+            (biscuit.utils.OUTCOME). Defaults to None.
+
+    Returns:
+        patient_auc, patient_ap, slide_auc, slide_ap, tile_auc, tile_ap, thresh
+    """
+    if outcome is None:
+        outcome = OUTCOME
     csv = pd.read_csv(join(path, 'results_log.csv'))
     model_res = next(csv.iterrows())[1]
     pt_ap = mean(eval(model_res['patient_ap'])[outcome])
@@ -39,16 +54,27 @@ def get_model_results(path, outcome=None):
     slide_auc = eval(model_res['slide_auc'])[outcome][0]
     tile_ap = mean(eval(model_res['tile_ap'])[outcome])
     tile_auc = eval(model_res['tile_auc'])[outcome][0]
-
-    predictions_path = join(path, f'patient_predictions_{OUTCOME}_val_epoch1.csv')
-    if os.path.exists(predictions_path):
-        manual_auc, opt_thresh = auc_and_threshold(*read_group_predictions(predictions_path))
+    pred_path = join(path, f'patient_predictions_{OUTCOME}_val_epoch1.csv')
+    if os.path.exists(pred_path):
+        _, opt_thresh = auc_and_threshold(*read_group_predictions(pred_path))
     else:
-        manual_auc, opt_thresh = None, None
-
+        _, opt_thresh = None, None
     return pt_auc, pt_ap, slide_auc, slide_ap, tile_auc, tile_ap, opt_thresh
 
+
 def find_cv_early_stop(P, label, k=3, outcome=None):
+    """Detects early stop batch from cross-val trained models.
+
+    Args:
+        P (slideflow.Project): Project.
+        label (str): Experimental label.
+        k (int, optional): Number of k-fold iterations. Defaults to 3.
+        outcome (str, optional): Outcome name. If none, uses default
+            (biscuit.utils.OUTCOME). Defaults to None.
+
+    Returns:
+        int: Early stop batch.
+    """
     cv_folders = find_cv(P, label, k=k, outcome=outcome)
     early_stop_batch = []
     for cv_folder in cv_folders:
@@ -58,55 +84,146 @@ def find_cv_early_stop(P, label, k=3, outcome=None):
             early_stop_batch += [model_res['early_stop_batch']]
     if len(early_stop_batch) == len(cv_folders):
         # Only returns early stop if it was triggered in all crossfolds
-        #TODO: consider if this is best approach or not
         return round(mean(early_stop_batch))
     else:
         return None
 
-# --- Utility functions for finding experiment models -----------------------------------------------------------------
+
+# --- Utility functions for finding experiment models -------------------------
 
 def find_model(P, label, epoch=None, kfold=None, outcome=None):
-    if outcome is None: outcome=OUTCOME
+    """Searches for a model in a project model directory.
+
+    Args:
+        P (slideflow.Project): Project.
+        label (str): Experimental label.
+        epoch (int, optional): Epoch to search for. If not None, returns
+            path to the saved model. If None, returns path to parent model
+            folder. Defaults to None.
+        kfold (int, optional): K-fold iteration. Defaults to None.
+        outcome (str, optional): Outcome name. If none, uses default
+            (biscuit.utils.OUTCOME). Defaults to None.
+
+    Raises:
+        MultipleModelsFoundError: If multiple potential matches are found.
+        ModelNotFoundError: If no matching model is found.
+
+    Returns:
+        str: Path to matching model.
+    """
+    if outcome is None:
+        outcome = OUTCOME
     tail = '' if kfold is None else f'-kfold{kfold}'
     model_name = f'{outcome}-{label}-HP0{tail}'
     matching = [o for o in os.listdir(P.models_dir) if o[6:] == model_name]
     if len(matching) > 1:
-        raise MultipleModelsFoundError(f"Multiple matching model experiments found matching {model_name}")
+        msg = f"Multiple matching models found matching {model_name}"
+        raise MultipleModelsFoundError(msg)
     elif not len(matching):
-        raise ModelNotFoundError(f"No matching model found matching {model_name}.")
+        msg = f"No matching model found matching {model_name}."
+        raise ModelNotFoundError(msg)
     elif epoch is not None:
-        return join(P.models_dir, matching[0], f'{outcome}-{label}-HP0{tail}_epoch{epoch}')
+        return join(
+            P.models_dir,
+            matching[0],
+            f'{outcome}-{label}-HP0{tail}_epoch{epoch}'
+        )
     else:
         return join(P.models_dir, matching[0])
 
+
 def model_exists(P, label, epoch=None, kfold=None, outcome=None):
+    """Check if matching model exists.
+
+    Args:
+        P (slideflow.Project): Project.
+        label (str): Experimental label.
+        epoch (int, optional): Epoch number of saved model. Defaults to None.
+        kfold (int, optional): K-fold iteration. Defaults to None.
+        outcome (str, optional): Outcome name. If none, uses default
+            (biscuit.utils.OUTCOME). Defaults to None.
+
+    Returns:
+        bool: If model exists
+    """
     try:
         find_model(P, label, epoch, kfold=kfold, outcome=outcome)
         return True
     except ModelNotFoundError:
         return False
 
+
 def find_cv(P, label, epoch=None, k=3, outcome=None):
-    return [find_model(P, label, epoch=epoch, kfold=_k, outcome=outcome) for _k in range(1, k+1)]
+    """Finds paths to cross-validation models.
+
+    Args:
+        P (slideflow.Project): Project.
+        label (str): Experimental label.
+        epoch (int, optional): Epoch number of saved model. Defaults to None.
+        kfold (int, optional): K-fold iteration. Defaults to None.
+        outcome (str, optional): Outcome name. If none, uses default
+            (biscuit.utils.OUTCOME). Defaults to None.
+
+    Returns:
+        list(str): Paths to cross-validation models.
+    """
+    return [
+        find_model(P, label, epoch=epoch, kfold=_k, outcome=outcome)
+        for _k in range(1, k+1)
+    ]
+
 
 def find_eval(P, label, epoch=1, outcome=None):
-    if outcome is None: outcome=outcome
-    matching = [o for o in os.listdir(P.eval_dir) if o[11:] == f'{OUTCOME}-{label}-HP0_epoch{epoch}']
+    """Finds matching eval directory.
+
+    Args:
+        P (slideflow.Project): Project.
+        label (str): Experimental label.
+        epoch (int, optional): Epoch number of saved model. Defaults to None.
+        outcome (str, optional): Outcome name. If none, uses default
+            (biscuit.utils.OUTCOME). Defaults to None.
+
+    Raises:
+        MultipleModelsFoundError: If multiple matches are found.
+        ModelNotFoundError: If no match is found.
+
+    Returns:
+        str: path to eval directory
+    """
+    if outcome is None:
+        outcome = outcome
+    matching = [
+        o for o in os.listdir(P.eval_dir)
+        if o[11:] == f'{OUTCOME}-{label}-HP0_epoch{epoch}'
+    ]
     if len(matching) > 1:
-        raise MultipleModelsFoundError(f"Multiple matching eval experiments found for label {label}")
+        msg = f"Multiple matching eval experiments found for label {label}"
+        raise MultipleModelsFoundError(msg)
     elif not len(matching):
         raise ModelNotFoundError(f"No matching eval found for label {label}")
     else:
         return join(P.eval_dir, matching[0])
 
+
 def eval_exists(P, label, epoch=1):
+    """Check if matching eval exists.
+
+    Args:
+        P (slideflow.Project): Project.
+        label (str): Experimental label.
+        epoch (int, optional): Epoch number of saved model. Defaults to None.
+
+    Returns:
+        bool: If eval exists
+    """
     try:
         find_eval(P, label, epoch)
         return True
     except ModelNotFoundError:
         return False
 
-# --- Thresholding and metrics functions ------------------------------------------------------------------------------
+
+# --- Thresholding and metrics functions --------------------------------------
 
 def load_and_fix_patient_pred(path):
     '''Reads patient-level predictions CSV file, returning pandas dataframe'''
@@ -123,26 +240,36 @@ def load_and_fix_patient_pred(path):
         shutil.move(path, path+'.backup')
         with open(path+'.backup', 'r') as backup:
             reader = csv.reader(backup)
-            old_header = next(reader)
+            _ = next(reader)
             with open(path, 'w') as fixed:
                 writer = csv.writer(fixed)
                 writer.writerow(header)
                 for row in reader:
                     writer.writerow(row)
-
     return pd.read_csv(path)
 
-def read_group_predictions(path):
-    '''Reads patient- or slide-level predictions CSV file, returning y_true and y_pred'''
 
-    # Read into pandas dataframe
+def read_group_predictions(path):
+    '''Reads patient- or slide-level predictions CSV file,
+    returning y_true and y_pred
+    '''
     df = load_and_fix_patient_pred(path)
-    level = 'patient' if 'patient' in df.columns else 'slide'
     y_true = df['y_true1'].to_numpy()
     y_pred = df['percent_tiles_positive1'].to_numpy()
     return y_true, y_pred
 
+
 def prediction_metrics(y_true, y_pred, threshold):
+    """Calculate prediction metrics (AUC, sensitivity/specificity, etc)
+
+    Args:
+        y_true (np.ndarray): True labels.
+        y_pred (np.ndarray): Predictions.
+        threshold (_type_): Prediction threshold.
+
+    Returns:
+        dict: Prediction metrics.
+    """
     yt = y_true.astype(bool)
     yp = y_pred > threshold
 
@@ -157,7 +284,7 @@ def prediction_metrics(y_true, y_pred, threshold):
     specificity = tn / (tn + fp)
 
     # Youden's confidence interval, via BAC (bootstrap AC estimate)
-    # Bootstrapping is performed with sample size n = 100 and iterations B = 500
+    # Bootstrapping performed with sample size n = 100 and iterations B = 500
     all_jac = []
     for _ in range(500):
         bootstrap_i = np.random.choice(np.arange(yt.shape[0]), size=(150,))
@@ -167,8 +294,10 @@ def prediction_metrics(y_true, y_pred, threshold):
         _fp = np.logical_and(np.logical_not(_yt), _yp).sum()
         _tn = np.logical_and(np.logical_not(_yt), np.logical_not(_yp)).sum()
         _fn = np.logical_and(_yt, np.logical_not(_yp)).sum()
-        _jac = ((_tn + 0.5 * z**2) / (_tn + _fp + z**2)) - ((_fn + 0.5 * z**2) / (_fn + _tp + z**2))
+        _jac = (((_tn + 0.5 * z**2) / (_tn + _fp + z**2))
+                - ((_fn + 0.5 * z**2) / (_fn + _tp + z**2)))
         all_jac += [_jac]
+
     jac = mean(all_jac)
     jac_var = variance(all_jac)
     jac_low = jac - z * np.sqrt(jac_var)
@@ -176,7 +305,7 @@ def prediction_metrics(y_true, y_pred, threshold):
 
     # AUC confidence intervals
     if not np.array_equal(np.unique(y_true), [0, 1]):
-        sf.util.log.warn(f"Unable to calculate CI; NaNs exist")
+        sf.util.log.warn("Unable to calculate CI; NaNs exist")
         ci = [None, None]
     else:
         delong_auc, auc_cov = delong_roc_variance(y_true, y_pred)
@@ -196,13 +325,35 @@ def prediction_metrics(y_true, y_pred, threshold):
         'youden_high': jac_high,
     }
 
+
 def auc_and_threshold(y_true, y_pred):
+    """Calculates AUC and optimal threshold (via Youden's J)
+
+    Args:
+        y_true (np.ndarray): Y true (labels).
+        y_pred (np.ndarray): Y pred (predictions).
+
+    Returns:
+        float: AUC
+        float: Optimal threshold
+    """
     fpr, tpr, threshold = metrics.roc_curve(y_true, y_pred)
     roc_auc = metrics.auc(fpr, tpr)
-    optimal_threshold = threshold[list(zip(tpr,fpr)).index(max(zip(tpr, fpr), key=lambda x: x[0]-x[1]))]
+    max_j = max(zip(tpr, fpr), key=lambda x: x[0]-x[1])
+    optimal_threshold = threshold[list(zip(tpr, fpr)).index(max_j)]
     return roc_auc, optimal_threshold
 
+
 def auc(y_true, y_pred):
+    """Calculate Area Under Receiver Operator Curve (AUC / AUROC)
+
+    Args:
+        y_true (np.ndarray): True labels.
+        y_pred (np.ndarray): Predictions.
+
+    Returns:
+        Float: AUC
+    """
     try:
         fpr, tpr, threshold = metrics.roc_curve(y_true, y_pred)
         return metrics.auc(fpr, tpr)
